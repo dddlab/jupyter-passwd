265220113d6f288e7006adc347c70972b8cd9b85
Sang-Yun Oh <syoh@ucsb.edu>
2020-07-20 11:34:01 -0700

    update message prompt

--- hash-password.py
+++ hash-password.py
@@ -2,4 +2,6 @@
 # https://jupyter-notebook.readthedocs.io/en/stable/public_server.html#preparing-a-hashed-password
 
 from notebook.auth import passwd
+print('Enter a password to be hashed')
+print('For details, refer to this documentation: https://jupyter-notebook.readthedocs.io/en/stable/public_server.html#preparing-a-hashed-password')
 print(passwd())
