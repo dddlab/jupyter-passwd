6b765fef5ea0adccbe08f3a03ff648d0f537f7f9
Sang-Yun Oh <syoh@ucsb.edu>
2020-10-23 23:44:24 -0700

    remove prompts

--- hash-password.py
+++ hash-password.py
@@ -2,7 +2,4 @@
 # https://jupyter-notebook.readthedocs.io/en/stable/public_server.html#preparing-a-hashed-password
 
 from notebook.auth import passwd
-print('See documentation about securing your Jupyter notebook server')
-print('https://jupyter-notebook.readthedocs.io/en/stable/public_server.html#preparing-a-hashed-password')
-print('Enter a password to hash:')
 print(passwd())
